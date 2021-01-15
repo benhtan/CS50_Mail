867601128c3ba5d0f10b850a1ffc95e6f8c4c94c
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2021-01-14 21:45:47 -0800

    send mail working

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,8 +6,44 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Find #recipient-error-msg and don't display it
+  document.querySelector('#recipient-error-msg').style.display = 'none';
+
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // event handler when new mail form is submitted
+  document.querySelector('#compose-form').onsubmit = function() {
+    console.log(document.querySelector('#compose-recipients').value);
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+
+        // if no error
+        if (result.error === undefined){
+          load_mailbox('sent');
+        } else {
+          document.querySelector('#recipient-error-msg').innerHTML = result.error;
+          document.querySelector('#recipient-error-msg').style.display = 'block';
+          document.querySelector('#compose-recipients').focus();
+        }
+    })
+    .catch(error => {
+      console.log('Error: ', error);
+    });
+
+    return false;
+  };
 });
 
 function compose_email() {
@@ -23,7 +59,7 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+ 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
